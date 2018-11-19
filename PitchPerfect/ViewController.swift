c95eb5c071067ff478c9312cf4cd06ec4c57b6f8
seanlin618 <teli5023@colorado.edu>
2018-11-20 01:47:18 +0800

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Sean Lin on 2018/11/20.
+//  Copyright © 2018 Sean Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
